adf7c1fa189b4e19376f3ef649abf0d8393fb098
tuduongau <jonhnny168@gmail.com>
2024-06-07 11:50:15 +0700

    Correcting page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <a className="font-sans hover:font-serif mb-3 text-2xl ">
+            <h1 className="font-sans hover:font-serif mb-3 text-2xl ">
               Chonk Chonk
-            </a>
+            </h1>
             <Image
               src="/chonker.png"
               alt="Johnny What's Up"
@@ -31,7 +31,7 @@ export default function Home() {
 
       <div>
         <a
-          href="https://www.youtube.com/watch?v=s9oQRKsROF8&t=1s" 
+          href="https://www.youtube.com/watch?v=s9oQRKsROF8&t=1s"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -98,23 +98,23 @@ export default function Home() {
           </p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+
+        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        target="_blank"
+        rel="noopener noreferrer"
+
+        <h2 className="mb-3 text-2xl font-semibold">
+          Deploy{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+          Instantly deploy your Next.js site to a shareable URL with Vercel.
+        </p>
       </div>
+
     </main>
   );
 }
